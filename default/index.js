ae63dfda53edf5df94f942d3e1fa719f4986bcd9
Siam Mridha <masudalamsiam@gmail.com>
2020-02-19 03:12:10 -0500

    no message

--- default/index.js
+++ default/index.js
@@ -7,12 +7,16 @@ exports.handler = async (event) => {
     const socket = await WebSocket(event)
     switch (action) {
       case 'call':
-        return socket.emitTo(client, action, data)
+        return await socket.emitTo(client, action, data)
       case 'call-accept':
-        return socket.emitTo(client, action, data)
+        return await socket.emitTo(client, action, data)
       case 'call-decline':
-        return socket.emitTo(client, action, data)
+        return await socket.emitTo(client, action, data)
+      case 'get-users':
+        const { Items } = await socket.db('scan', { TableName: process.env.TABLE_NAME })
+        return res.send({ action: `${action}call_back`, data: { ok: true, users: Items } })
     }
+    throw new Error('unknown action')
   } catch (error) {
     console.log(error)
     return res.status(500).send({ action: `${action}call_back`, data: { ok: false, message: error.message } })
--- disconnect/index.js
+++ disconnect/index.js
@@ -4,9 +4,9 @@ const WebSocket = require('web-socket-server')
 exports.handler = async (event) => {
   try {
     const socket = await WebSocket(event)
-    return await socket.emit('user-disconnected', {})
+    return await socket.emit('user-disconnected', { name: socket.clientName })
   } catch (error) {
     console.log(error)
-    return res.status(500).send({ action: `${event.requestContext.connectionId}call_back`, data: { ok: false, message: error.message } })
+    return res.status(500).send({ ok: false, message: error.message })
   }
 }
