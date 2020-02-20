0eee64ba95c0094adcd6c9c7041ccc80dd99516d
Siam Mridha <masudalamsiam@gmail.com>
2020-02-20 04:15:36 -0500

    no message

--- default/index.js
+++ default/index.js
@@ -2,23 +2,20 @@ const res = require('response')
 const WebSocket = require('web-socket-server')
 
 exports.handler = async (event) => {
-  const { client, action, data } = JSON.parse(event.body)
+  console.log(event)
+  const { action, data } = JSON.parse(event.body)
   try {
     const socket = await WebSocket(event)
     switch (action) {
-      case 'call':
-        return await socket.emitTo(client, action, data)
-      case 'call-accept':
-        return await socket.emitTo(client, action, data)
-      case 'call-decline':
-        return await socket.emitTo(client, action, data)
       case 'get-users':
         const { Items } = await socket.db('scan', { TableName: process.env.TABLE_NAME })
-        return res.send({ action: `${action}call_back`, data: { ok: true, users: Items } })
+        return res.send({ action: data.call_back, data: { ok: true, users: Items } })
+      default:
+        const client = event.requestContext.connectionId
+        await socket.emitTo(data.client, action, { ...data, client })
     }
-    throw new Error('unknown action')
   } catch (error) {
     console.log(error)
-    return res.status(500).send({ action: `${action}call_back`, data: { ok: false, message: error.message } })
+    return res.status(500).send(error.message)
   }
 }
